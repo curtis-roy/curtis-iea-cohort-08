d3ddce7613a70d6e67f459272710aff97f17f2c1
curtis-roy <curtis.roy@libertymutual.com>
2022-10-18 14:20:54 -0700

    Add updates from lab work 10-18-2022

--- 06_rest/dark_sky_api_lab.py
+++ 06_rest/dark_sky_api_lab.py
@@ -17,15 +17,13 @@
 from pprint import pprint
 import requests
 
-SECRET_KEY = '1d8c58ed1d54f96f939e706c788650f1'
-lat , long = (47.67, -117.40) # Spokane, WA
-response = requests.get('https://api.darksky.net/forecast/{key}/{lat},{long}'.format(key=SECRET_KEY, lat=lat, long=long))
+SECRET_KEY = "1d8c58ed1d54f96f939e706c788650f1"
+lat, long = (47.67, -117.40)  # Spokane, WA
+response = requests.get(f"https://api.darksky.net/forecast/{SECRET_KEY}/{lat},{long}")
 
 response_dict = response.json()
 
 pprint(response_dict)
-
-
 
 # for row in response:
 #     print(row[lat],row[long])
