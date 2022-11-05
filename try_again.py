a3c13c218f4f58af731b46a260baddd1b730d6d5
curtis-roy <curtis.roy@libertymutual.com>
2022-11-05 15:50:34 -0700

    Add updates to try_again.py

--- try_again.py
+++ try_again.py
@@ -1,20 +1,20 @@
 #!/bin/env python3
 
 """ If I remember correctly, this was the gist:
-    Look at a list of 3 items.  Ignore the first item (position 0).
-    If the item at position 1 == APPLY, then print the last item in the list.
+    Look at a list of 3 items.  Ignore the first item.
+    If the middle item == APPLY, then print the last item in the list.
     Change the last item in the list, then print again with the new one."""
 
 def tryagain():
-    sentence = []
-    for i in elements:
-        if elements[1] == "APPLY":
-            sentence.append(elements[-1])
-            print(sentence)
+    del elements[0] # Get rid of item at pos 0 since we're not using it anyway
+    empty = [] # an empty list to put things into
+    if elements[0] == 'APPLY': # meeting the specified condition
+        for item in elements:
+            empty.append(elements[-1]) # add last item in elements to empty list
+            elements[-1] = values[0] # set last item in elements to new value from values
+    print(empty)
 
 elements = [0, 'APPLY', 'Hey']
-values = ['Hey', 'there', '!']
+values = ['there']
 
 tryagain()
-
-# JR off the cuff idea print(fields[-1] if fields[1] == "APPLY" else "")
